62fc0f5bd0066cfaeb247486fa36a40033c4806c
zforster <zak.forster16@gmail.com>
2020-11-08 20:59:49 +0000

    working room and new user joining msg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import io from 'socket.io-client';
-const socket = io('http://localhost:4000', {"transports" : ["websocket"]});
+const socket = io('http://localhost:4000/games', {"transports" : ["websocket"]});
 
 function App() {
+    socket.emit("joinRoom", "test_room");
 
-    console.log("test");
-    socket.on("welcome", (data) => {
+    socket.on("success", (data) => {
         console.log(data);
+    });
+
+    socket.on("newUser", (data) => {
+       console.log(data);
     });
 
     return(
