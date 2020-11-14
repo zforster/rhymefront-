cb45ec806f90b4e8a01e7319ee71e38f1091ba2f
zforster <zak.forster16@gmail.com>
2020-11-14 19:53:13 +0000

    add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Home } from './pages/home';
+import { JoinGame } from './pages/joinGame';
+import {Route, BrowserRouter as Router} from "react-router-dom";
 
 
 function App() {
     return (
-        <Home/>
+        <Router>
+            <Route exact path="/:room" render={(props) => <JoinGame room={props.match.params.room}/>}/>
+            <Route exact path="/" component={Home}/>
+        </Router>
     )
 }
 
