87eb9859eedd7481e2e5873d55576b0e5fe9cafd
zforster <zak.forster16@gmail.com>
2020-11-14 23:37:06 +0000

    test disconnect

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useEffect }  from 'react';
 import { useStore } from "../state/store";
+import { userJoinedRoom, disconnectSocket, userLeftRoom, leaveRoom } from "../socket/socket";
 import { UsernameInputField } from '../components/inputs';
 import { JoinRoomButton } from '../components/buttons';
 
 
 export function Home(props) {
-    document.title = "Join Game | Rhyme With Friends";
+    document.title = `${props.title} | Rhyme With Friends`;
 
     const username = useStore(state => state.username);
     const roomName = useStore(state => state.roomName);
+
+    useEffect(() => {
+        userJoinedRoom((data) => console.log(data));
+        userLeftRoom((data) => console.log(data));
+
+        return () => {
+            leaveRoom(roomName);
+            // disconnectSocket();
+        }
+    }, []);
 
     if(roomName === "") {
         return (
             <div>
                 <UsernameInputField/>
-                <JoinRoomButton roomName={props.room} message={props.buttonText}/>
+                <JoinRoomButton roomName={props.room} message={props.btnText}/>
             </div>
         )
     }
