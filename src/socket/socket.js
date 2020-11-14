e6ee9b40cc47fb2d48a87f46d050d31957b1d37e
zforster <zak.forster16@gmail.com>
2020-11-14 23:36:49 +0000

    add new socket funcs

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,12 +1,28 @@
 import { io } from 'socket.io-client';
 
 
-const socket = io('http://localhost:4000/games', {"transports" : ["websocket"]});
+export const socket = io('http://localhost:4000/games', {"transports" : ["websocket"]});
 
 export const joinRoom = (room, username) => {
     socket.emit('joinRoom', {room: room, username: username});
 };
 
-export const disconnect = () => {
+export const userJoinedRoom = (data) => {
+    socket.on('userJoinedRoom', msg => {
+        return data(msg);
+    });
+};
+
+export const userLeftRoom = (data) => {
+    socket.on('userLeftRoom', msg => {
+        return data(msg);
+    });
+};
+
+export const leaveRoom = (room) => {
+    socket.emit('leaveRoom', {room: room});
+};
+
+export const disconnectSocket = () => {
     socket.disconnect();
 };
