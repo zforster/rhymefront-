d560c55ba21ebe3a1eef07224c3f305234058c05
zforster <zak.forster16@gmail.com>
2020-11-15 19:10:38 +0000

    add prop args

--- src/components/inputs.js
+++ src/components/inputs.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { TextField } from '@material-ui/core';
 import {useStore} from "../state/store";
 
-export function UsernameInputField(){
+export function UsernameInputField(props){
     const setUsername = useStore(state => state.setUsername);
     return (
-        <TextField
-            inputProps={{ maxLength: 16 }}
-            id="outlined-basic"
-            label="Nickname"
-            variant="outlined"
-            autoFocus={true}
-            size="small"
-            onChange={(event) => setUsername(event.target.value)}
-        />
+        <div {...props}>
+            <TextField
+                inputProps={{ maxLength: 16 }}
+                id="outlined-basic"
+                label="Nickname"
+                variant="outlined"
+                autoFocus={true}
+                size="small"
+                onChange={(event) => setUsername(event.target.value)}
+            />
+        </div>
     )
 }
