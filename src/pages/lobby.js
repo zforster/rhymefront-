d0f0c3d1fa21972bee83fcd011de02d9cb89fd88
zforster <zak.forster16@gmail.com>
2020-11-15 22:13:48 +0000

    add join link

--- src/pages/lobby.js
+++ src/pages/lobby.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useStore } from "../state/store";
-
+import { Title } from "../components/title";
+import { GridContainer } from "../components/grid";
+import TextField from '@material-ui/core/TextField';
 
 export function Lobby() {
     document.title = `Lobby | Rhyme With Friends`;
@@ -13,10 +15,26 @@ export function Lobby() {
     const players = currentPlayers.map((data) => {return <li key={Math.random()}>{data.username}</li>});
 
     return (
-        <div>
-            <h1>Your Username: {username}</h1>
-            <h1>Current Players: {players}</h1>
-            <h1>Your Room: {roomName}</h1>
+        <div style={{display: "flex", flexDirection: "column", flex: 1, backgroundColor: "#f5f5f5"}}>
+            <Title/>
+            <GridContainer>
+                <div>
+                    <h1>Your Username: {username}</h1>
+                    <h1>Current Players: {players}</h1>
+                    <h1>Your Room: {roomName}</h1>
+                    <TextField
+                        id="standard-full-width"
+                        label="Label"
+                        style={{ margin: 8 }}
+                        value={`http://localhost:3000/${roomName}`}
+                        fullWidth
+                        margin="normal"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                </div>
+            </GridContainer>
         </div>
     )
 }
