05f2d30badaea43b432490c6c71acffd232f884c
zforster <zak.forster16@gmail.com>
2020-11-15 17:05:28 +0000

    store current player base in the room

--- src/state/store.js
+++ src/state/store.js
@@ -4,6 +4,10 @@ export const useStore = create(set => ({
     username: "",
     setUsername: (username) => set({username: username}),
 
+    currentPlayers: [],
+    addToCurrentPlayers: (player) => set(state => ({currentPlayers: [...state.currentPlayers, player]})),
+    removeFromCurrentPlayers: (player) => set(state => ({currentPlayers: state.currentPlayers.filter(function(el){return el.id !== player.id})})),
+
     roomName: "",
     setRoomName: (room) => set({roomName: room})
 }));
