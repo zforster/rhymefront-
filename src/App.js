2e1d88b537fcee9fe0165360a3f6818cbf7ec83a
zforster <zak.forster16@gmail.com>
2020-11-15 19:11:40 +0000

    use same home component for joining lobby and creating lobby

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import {Route, BrowserRouter as Router} from "react-router-dom";
 function App() {
     return (
         <Router>
-            <Route exact path="/:room" render={(props) => <Home room={props.match.params.room} btnText={"JOIN GAME"} title={"Join Game"}/>}/>
-            <Route exact path="/" render={() => <Home room={null} btnText={"CREATE PRIVATE GAME"} title={"Home"}/>}/>
+            <Route exact path="/:room" render={(props) => <Home room={props.match.params.room} message={"JOIN GAME"} title={"Join Game"}/>}/>
+            <Route exact path="/" render={() => <Home room={null} message={"CREATE PRIVATE GAME"} title={"Home"}/>}/>
         </Router>
     )
 }
