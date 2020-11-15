39d2485f2c45c225b09c4acf943fcc4b2e30c326
zforster <zak.forster16@gmail.com>
2020-11-15 17:05:14 +0000

    register when user enters or leaves a room

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import React, { useEffect }  from 'react';
 import { useStore } from "../state/store";
-import { userJoinedRoom, disconnectSocket, userLeftRoom, leaveRoom } from "../socket/socket";
+import { userJoinedRoom, disconnectSocket, userLeftRoom } from "../socket/socket";
 import { UsernameInputField } from '../components/inputs';
 import { JoinRoomButton } from '../components/buttons';
 
@@ -11,15 +11,23 @@ export function Home(props) {
     const username = useStore(state => state.username);
     const roomName = useStore(state => state.roomName);
 
+    const currentPlayers = useStore(state => state.currentPlayers);
+    const addToCurrentPlayers = useStore(state => state.addToCurrentPlayers);
+    const removeFromCurrentPlayers = useStore(state => state.removeFromCurrentPlayers);
+
     useEffect(() => {
-        userJoinedRoom((data) => console.log(data));
-        userLeftRoom((data) => console.log(data));
+        userJoinedRoom((data) => {
+            addToCurrentPlayers(data);
+        });
+
+        userLeftRoom((data) => {
+            removeFromCurrentPlayers(data);
+        });
 
         return () => {
-            leaveRoom(roomName);
-            // disconnectSocket();
+            disconnectSocket();
         }
-    }, []);
+    }, [addToCurrentPlayers, removeFromCurrentPlayers]);
 
     if(roomName === "") {
         return (
@@ -30,10 +38,13 @@ export function Home(props) {
         )
     }
     else{
+        console.log(currentPlayers);
+        let players = currentPlayers.map((data) => {return <li key={Math.random()}>{data.username}</li>});
         return (
             <div>
-                <h1>{username}</h1>
-                <h1>{roomName}</h1>
+                <h1>Your Username: {username}</h1>
+                <h1>Current Players: {players}</h1>
+                <h1>Your Room: {roomName}</h1>
             </div>
         )
     }
