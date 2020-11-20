2ccd7616f0fbe14ada7195e93644e66648444aa3
zforster <zak.forster16@gmail.com>
2020-11-20 21:30:30 +0000

    only enable game start on the hosts screen

--- src/pages/lobby.js
+++ src/pages/lobby.js
@@ -5,16 +5,25 @@ import { GridContainer } from "../components/grid";
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
+
+function disableButton(id, hostId){
+    return !(id === hostId);
+}
+
 export function Lobby() {
     document.title = `Lobby | Rhyme With Friends`;
 
     const [showCopied, setShowCopied] = useState(false);
+
     const username = useStore(state => state.username);
     const roomName = useStore(state => state.roomName);
 
+    const id = useStore(state => state.id);
+    const hostId = useStore(state => state.hostId);
+
     const currentPlayers = useStore(state => state.currentPlayers);
 
-    const players = currentPlayers.map((data) => {return <PlayerInLobby text={data.username}/>});
+    const players = currentPlayers.map((data, i) => {return <PlayerInLobby key={i} text={data.username}/>});
 
     const styleBackground = showCopied ? "#f5f5f5" : "white";
     const style = { textAlign: 'center', cursor: "pointer", backgroundColor: styleBackground};
@@ -26,7 +35,7 @@ export function Lobby() {
                 <div>
                     <PlayersInLobbyTitle text={"Players"}/>
                     {players}
-                    <Button style={{marginBottom: "3rem", marginTop: "2.6rem"}} variant="contained" color="primary">Start Game</Button>
+                    <Button style={{marginBottom: "3rem", marginTop: "2.6rem"}} variant="contained" color="primary" disabled={disableButton(id, hostId)}>Start Game</Button>
                     <TextField
                         onClick={() => {
                             navigator.clipboard.writeText(`http://localhost:3000/${roomName}`);
@@ -39,7 +48,7 @@ export function Lobby() {
                         value={`http://localhost:3000/${roomName}`}
                         fullWidth
                         variant="outlined"
-                        inputProps={{shrink: true, readOnly: true, style: style}}
+                        inputProps={{shrink: "true", readOnly: true, style: style}}
                     />
                 </div>
             </GridContainer>
