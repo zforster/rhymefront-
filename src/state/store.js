575c183203a33f05b883b8624d3a86515724d0b5
zforster <zak.forster16@gmail.com>
2020-11-20 21:32:21 +0000

    add set id and set host id functions

--- src/state/store.js
+++ src/state/store.js
@@ -4,6 +4,12 @@ export const useStore = create(set => ({
     username: "",
     setUsername: (username) => set({username: username}),
 
+    id: "",
+    setId: (id) => set({id: id}),
+
+    hostId: "",
+    setHostId: (id) => set({hostId: id}),
+
     currentPlayers: [],
     addToCurrentPlayers: (player) => set(state => ({currentPlayers: [...state.currentPlayers, player]})),
     removeFromCurrentPlayers: (player) => set(state => ({currentPlayers: state.currentPlayers.filter(function(el){return el.id !== player.id})})),
